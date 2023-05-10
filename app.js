91444fa116c57f0fd7017dc16c3a97168a3e4738
elionavarroh <elionavarroh@gmail.com>
2023-05-10 16:28:17 +0200

    add all the inputs for the products and the respective operations

--- app.js
+++ app.js
@@ -63,8 +63,6 @@ const products = [
     },
 ];
 
-//Lista de productos
-
 var container = document.getElementById("product-list-container");
 
 var createCalculate = (product) => {
@@ -73,24 +71,46 @@ var createCalculate = (product) => {
     calculate.setAttribute("name", "calculate" + product.description);
     calculate.setAttribute("min", "0")
     calculate.setAttribute("max", product.stock);
-    calculate.setAttribute("change", event => products.units = event.target.value);
+    calculate.setAttribute("id", product.description);
+    calculate.addEventListener("change", event => product.units = parseInt(event.target.value));
     return calculate;
-}
+};
 
 var createDescription = (product) => {
     var description = document.createElement("h5");
     description.innerText = product.description + " - " + product.price + "€/ud.";
     container.appendChild(description);
-    description.appendChild(createCalculate(products));
-}
+    description.appendChild(createCalculate(product));
+};
 
 var showProducts = (productList) => {
     for (var product of productList) {
         createDescription(product);
     }
-}
+};
 
 showProducts(products);
 
+//operations
 
+var calculation = () => {
+    var total = 0
+    var subtotal = 0
+    var iva = 0
+    for (var product of products) {
+        subtotal = (product.units * product.price) + subtotal;
+        iva += ((product.units * product.price) * product.tax)/100 
+    }
+    total = subtotal + iva
+    console.log(total);
+    console.log(iva);
+    console.log(subtotal);
+};
 
+//events 
+document.getElementById("calculate").addEventListener("click", calculation);
+
+//pantalla
+document.getElementById("subtotal").innerText = "Subtotal"
+document.getElementById("iva").innerText = "Iva"
+document.getElementById("total").innerText = "Total"
